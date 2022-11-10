fe5d7616f9f87125e1172ed6d46e298ef26379ad
sobotapl <piosoboci@gmail.com>
2022-11-10 19:26:48 +0100

    initial commit

---
+++ src/main/java/pl/sobota/imagesenderspringboot/ImageSenderSpringbootApplication.java
@@ -0,0 +1,11 @@
+package pl.sobota.imagesenderspringboot;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class ImageSenderSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSenderSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/sobota/imagesenderspringboot/ImageSenderSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sobota.imagesenderspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSenderSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
